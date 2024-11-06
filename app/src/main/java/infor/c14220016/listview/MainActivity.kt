e9b3e96178b4be7f0bf78009ce4c4471ed540705
FarrellYunanto <farrellyunanto@gmail.com>
2024-11-06 20:07:22 +0700

    fix

--- app/src/main/java/infor/c14220016/listview/MainActivity.kt
+++ app/src/main/java/infor/c14220016/listview/MainActivity.kt
@@ -4,43 +4,46 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.SearchView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import infor.c14220016.listview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        enableEdgeToEdge()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+//        val binding = MainActivity.
 
         var data = mutableListOf<String>()
         data.addAll(listOf("1", "2", "3", "4", "5"))
 
-        val lvlAdapter : ArrayAdapter<String> = ArrayAdapter(
+        val lvlAdapter: ArrayAdapter<String> = ArrayAdapter(
             this,
             android.R.layout.simple_list_item_1,
             data
         )
 
+        binding.lv1.adapter = lvlAdapter
+
         val _lv1 = findViewById<ListView>(R.id.lv1)
         _lv1.adapter = lvlAdapter
 
         _lv1.setOnItemClickListener { parent, view, position, id ->
-            Toast.makeText(this, "${data[position]}",
-                Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                this, "${data[position]}",
+                Toast.LENGTH_LONG
+            ).show()
         }
 
         val _btnTambah = findViewById<Button>(R.id.btnTambah)
         _btnTambah.setOnClickListener {
-            var dtAkhir = Integer.parseInt(data.get(data.size-1))+1
+            var dtAkhir = Integer.parseInt(data.get(data.size - 1)) + 1
             data.add(dtAkhir.toString())
             lvlAdapter.notifyDataSetChanged()
         }
@@ -50,5 +53,20 @@ class MainActivity : AppCompatActivity() {
             data.removeFirst()
             lvlAdapter.notifyDataSetChanged()
         }
+
+        binding.svw.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                lvlAdapter.getFilter().filter(query)
+                return  false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                lvlAdapter.getFilter().filter(newText)
+                return false
+            }
+        })
+
+
+
     }
 }
